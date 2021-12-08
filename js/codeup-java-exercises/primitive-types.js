85300269abc9197e62215d904811d47d88995fe0
Scott Carpenter <carpenter.scott22@gmail.com>
2021-12-08 15:17:40 -0600

    identified data types

---
+++ js/codeup-java-exercises/primitive-types.js
@@ -0,0 +1,22 @@
+// Identify the data type of the following values
+
+var value1 = 99.9
+console.log(value1 + ' is a ' + typeof value1);
+
+var value2 = "'false'";
+console.log(value2 + ' is a ' + typeof value2);
+
+var value3 = false;
+console.log(value3 + ' is a ' + typeof value3);
+
+var value4 = '"0"';
+console.log(value4 + ' is a ' + typeof value4);
+
+var value5 = 0;
+console.log(value5 + ' is a ' + typeof value5);
+
+var value6 = true;
+console.log(value6 + ' is a ' + typeof value6);
+
+var value7 = '"true"';
+console.log(value7 + ' is a ' + typeof value7);
