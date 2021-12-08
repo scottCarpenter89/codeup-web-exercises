eaa8de849ec0f2df02a428455277c7ea3982684a
Scott Carpenter <carpenter.scott22@gmail.com>
2021-12-08 12:00:26 -0600

    added hello-asteroid

---
+++ js/hello-asteroid.js
@@ -0,0 +1,13 @@
+// console.log('hello, sweet asteroid');
+
+// console log the result of a string expression
+// which describes one of you favorite foods to eat
+// use the + operator to concatenate the separate strings
+
+// console.log("I love eating bbq brisket. " + "The savory and peppery flavors " + "combined with the lean and fatty textures is an amazing combination. " + "If it is prepared right, bbq sauce is considered an insult to the meat.");
+
+var favoriteFood = 'brisket';
+var whatFavoriteFood = 'My favorite food is ';
+var favoriteBecause = ', because it if the perfect combination of savoriness and texture.';
+
+console.log(whatFavoriteFood + favoriteFood + favoriteBecause);
