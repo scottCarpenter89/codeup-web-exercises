9de0c850d023338244c22a7ce58f01185ad07d04
Scott Carpenter <carpenter.scott22@gmail.com>
2021-12-08 12:34:24 -0600

    added food ascii art

--- js/hello-asteroid.js
+++ js/hello-asteroid.js
@@ -4,10 +4,10 @@
 // which describes one of you favorite foods to eat
 // use the + operator to concatenate the separate strings
 
-// console.log("I love eating bbq brisket. " + "The savory and peppery flavors " + "combined with the lean and fatty textures is an amazing combination. " + "If it is prepared right, bbq sauce is considered an insult to the meat.");
+// console.log("\nI love eating bbq brisket. \n" + "The savory and peppery flavors " + "combined with the lean and fatty textures is an amazing combination. \n" + "If it is prepared right, bbq sauce is considered an insult to the meat.\n");
 
 var favoriteFood = 'brisket';
-var whatFavoriteFood = 'My favorite food is ';
-var favoriteBecause = ', because it if the perfect combination of savoriness and texture.';
+var whatFavoriteFood = 'My \n\tfavorite \n\t\tfood \n\t\t\tis ';
+var favoriteBecause = '\n\t\t\t\t\tbecause \n\t\t\t\tit \n\t\t\tis \n\t\tthe \n\tperfect \ncombination \n\tof savoriness \n\t\tand texture.';
 
 console.log(whatFavoriteFood + favoriteFood + favoriteBecause);
