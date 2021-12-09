c1205f46083a0fb70e949b1c85de9dbfe9026190
Scott Carpenter <carpenter.scott22@gmail.com>
2021-12-09 09:02:54 -0600

    answered exercise questions

---
+++ js/codeup-java-exercises/operators.js
@@ -0,0 +1,25 @@
+// Predict the value of the following operations and execute them in the console
+
+// '1' + 2 ----> NaN
+console.log('1' + 2);
+
+// typeof '1' + 2 ----> NaN
+console.log(typeof '1' + 2);
+
+// 6 % 4 ----> 2
+console.log(6 % 4);
+
+// '3 + 4 is ' + 3 + 4 ----> NaN
+// // how could you make this produce the correct output?
+console.log('3 + 4 is ' + (3 + 4));
+
+// 0 < 0 ----> false
+console.log(0 < 0);
+
+// 'false' == false ----> true
+console.log('false' == false);
+
+// true == 'true' ----> true
+console.log(true == 'true');
+
+// 5 >= -5 ----> true
