27be261c0c9f79f15f0f820c9c42df574567464e
Scott Carpenter <carpenter.scott22@gmail.com>
2021-12-09 16:58:01 -0600

    completed part of exercise 3

--- js/codeup-java-exercises/datatypes-operators-variables.js
+++ js/codeup-java-exercises/datatypes-operators-variables.js
@@ -117,4 +117,38 @@ console.log(sample.substring(6, 12));
 // need help with this one
 
 
+/*
+Write some JavaScript code, that is, variables and operators, to describe the following scenarios. Do not worry about
+the real operations to get the values, the goal of these exercises is to understand how real world conditions can be
+represented with code.
+*/
 
+ /*You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it),
+ and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is $3, how much will
+  you have to pay?*/
+
+let littleMermaid = 3;
+let brotherBear = 5;
+let hercules = 1;
+let dailyRentalPrice = 3;
+
+let estimatedTotal = (littleMermaid * dailyRentalPrice) + (brotherBear * dailyRentalPrice) + (hercules * dailyRentalPrice);
+console.log('You can expected to pay about $' + estimatedTotal + ' dollars.');
+
+/*Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate
+ per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You
+ worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.*/
+
+let google = 400;
+let amazon = 380;
+let facebook = 350;
+
+let googleHours = 6;
+let amazonHours = 4;
+let facebookHours = 10;
+
+let payday = (google * googleHours) + (amazon * amazonHours) + (facebook * facebookHours);
+console.log(payday);
+
+/*A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with
+her current schedule.*/
