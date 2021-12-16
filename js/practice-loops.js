b6ffde0aac040577fc465a5ef69923f7c03e214c
Scott Carpenter <carpenter.scott22@gmail.com>
2021-12-16 17:05:45 -0600

    added alternate while solution

--- js/practice-loops.js
+++ js/practice-loops.js
@@ -3,16 +3,28 @@
 // let counter = 0; // starting point
 
 
-function countEvenNumbers() {
-    let counter = 0;
-    while (counter < 25){
-        counter += 1;
-        if(counter % 2 === 0) {
-            console.log(counter);
-        } else {
-            continue;
-        }
-    }
-}
+// function countEvenNumbers() {
+//     let counter = 0;
+//     while (counter < 25){
+//         counter += 1;
+//         if(counter % 2 === 0) {
+//             console.log(counter);
+//         } else {
+//             continue;
+//         }
+//     }
+// }
 
-countEvenNumbers();
+//countEvenNumbers();
+
+// alternate strategy
+
+// function altEvenCounter(){
+//     let counter = 0;
+//     while (counter < 25){
+//         console.log(counter); // notice how putting the log before the increment includes 0 and doesn't require the
+//         counter += 2;         // if as long as you increment by 2 and not 1
+//     }
+// }
+//
+// altEvenCounter();
