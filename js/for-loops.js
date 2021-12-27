4c421ff566d5c7dae0eb6bf5e0f86c6b11d4e5e6
Scott Carpenter <carpenter.scott22@gmail.com>
2021-12-27 14:41:22 -0600

    all but exercise 4 finished

--- js/for-loops.js
+++ js/for-loops.js
@@ -14,7 +14,7 @@ function showMultiplicationTable(number) {
 
 }
 
-console.log(showMultiplicationTable(4));
+// console.log(showMultiplicationTable(4));
 
 // 3. Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output
 // to the console whether each number is odd or even. For example:
@@ -38,8 +38,35 @@ function randomNumberGenerator() {
 
 // 4. Create a for loop that uses console.log to create the output shown below.
 
-function numberPrinter(){
-    // needs to log
-};
-
+function numberPrinter() {
+    // create a variable that counts from 1
+    let count = 1;
+    let logger = '';
+    // create a for loop that starts at 0, where the count is less than 10, and increments by 1
+    for (let i = 1; i < 10; i++) {
+        // if the count is less than to i,
+        if (count <= i) {
+            // then it will log the string version of the number and concatenate it to itself
+            // it will also add one to the count
+            logger += count.toString();
+            console.log(logger);
+            count++
+            // otherwise, if the count greater than i
+        } else {
+            // then it will increase the count by one and continue
+        }
+    }
+}
 numberPrinter();
+
+// 5. Create a for loop that uses console.log to create the output shown below.
+
+function decrementer(){
+
+    for (i = 100; 0 < i; i-=5){
+        console.log(i);
+    }
+}
+
+decrementer();
+
