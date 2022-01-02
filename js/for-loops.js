0af9f39269e6395321f65821e164f7f890d2ea8a
Scott Carpenter <carpenter.scott22@gmail.com>
2022-01-02 14:49:47 -0600

    finished exercise 4

--- js/for-loops.js
+++ js/for-loops.js
@@ -11,11 +11,11 @@ function showMultiplicationTable(number) {
         console.log(counter * number);
         counter++;
     } while (counter <= 10);
-        // return the product of the input number times the counter
+    // return the product of the input number times the counter
 
 }
 
-// console.log(showMultiplicationTable(4));
+console.log(showMultiplicationTable(4));
 
 // 3. Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output
 // to the console whether each number is odd or even. For example:
@@ -25,7 +25,7 @@ function randomNumberGenerator() {
     for (i = 0; i <= 10; i++) {
         // let the return variable equal a whole integer of a random number times the multiplier
         let randomNumberProduct = Math.floor(Math.random() * 200);
-        if (randomNumberProduct > 20 && randomNumberProduct % 2 === 0){
+        if (randomNumberProduct > 20 && randomNumberProduct % 2 === 0) {
             console.log(randomNumberProduct + ' is even.');
         } else if (randomNumberProduct > 20 && randomNumberProduct % 2 === 1) {
             console.log(randomNumberProduct + ' is odd.');
@@ -35,39 +35,32 @@ function randomNumberGenerator() {
     }
 }
 
-// randomNumberGenerator();
+randomNumberGenerator();
 
 // 4. Create a for loop that uses console.log to create the output shown below.
 
 function numberPrinter() {
-    // create a variable that counts from 1
-    let count = 1;
-    let logger = '';
-    // create a for loop that starts at 0, where the count is less than 10, and increments by 1
-    for (let i = 1; i < 10; i++) {
-        // if the count is less than to i,
-        if (count <= i) {
-            // then it will log the string version of the number and concatenate it to itself
-            // it will also add one to the count
-            logger += count.toString();
-            console.log(logger);
-            count++
-            // otherwise, if the count greater than i
-        } else {
-            // then it will increase the count by one and continue
+    for (let outer = 1; outer < 10; outer++) {
+        let logger = '';
+        for (let inner = 1; inner <= outer; inner++){
+            logger += outer;
         }
+        console.log(logger);
     }
 }
+
 numberPrinter();
 
 // 5. Create a for loop that uses console.log to create the output shown below.
 
-function decrementer(){
+function decrementer() {
 
-    for (i = 100; 0 < i; i-=5){
+    for (let i = 100; 0 < i; i -= 5) {
         console.log(i);
     }
 }
 
 decrementer();
+
+
 
