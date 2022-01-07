30a81bf01a3dd547ff0facd4596ed95b47a82d18
Scott Carpenter <carpenter.scott22@gmail.com>
2022-01-07 11:59:49 -0600

    deleted TODO items

--- js/circle.js
+++ js/circle.js
@@ -6,13 +6,11 @@
         radius: 3,
 
         getArea: function () {
-            // TODO: complete this method
             // hint: area = pi * radius^2
             return Math.PI * Math.pow(this.radius, 2); // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
-            // TODO: complete this method.
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
             let area;
@@ -32,7 +30,6 @@
     circle.logInfo(true);
 
     console.log("=======================================================");
-    // TODO: Change the radius of the circle to 5.
     circle.radius = 5;
 
     // log info about the circle
