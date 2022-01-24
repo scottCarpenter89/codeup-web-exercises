f7f84128ef1489793c261f8b65bb77d7956ef36a
Scott Carpenter <carpenter.scott22@gmail.com>
2022-01-23 22:01:46 -0600

    added tab content and created js file

---
+++ js/order-pizza.js
@@ -0,0 +1,8 @@
+$('#size-tab-btn').on('click', function (e) {
+    e.preventDefault()
+    $('#myTab li:nth-child(1) a').tab('show')
+})
+$('#to-size').on('click', function (e) {
+    e.preventDefault()
+    $('#myTab li:nth-child(1) a').tab('show')
+})
