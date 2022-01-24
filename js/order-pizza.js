08397f5f2d7dbbfb1fe7f32120e7422366a13d6a
Scott Carpenter <carpenter.scott22@gmail.com>
2022-01-23 22:16:51 -0600

    experimenting with tab functionality

--- js/order-pizza.js
+++ js/order-pizza.js
@@ -2,7 +2,22 @@ $('#size-tab-btn').on('click', function (e) {
     e.preventDefault()
     $('#myTab li:nth-child(1) a').tab('show')
 })
-$('#to-size').on('click', function (e) {
+$('#cheese-sauce-tab').on('click', function (e) {
     e.preventDefault()
     $('#myTab li:nth-child(1) a').tab('show')
 })
+$('#cheese-tab-btn').on('click', function (e) {
+    e.preventDefault()
+    $('#myTab li:nth-child(2) a').tab('show')
+})
+$('#to-cheese').on('click', function (e) {
+    e.preventDefault()
+    $('#myTab li:nth-child(2) a').tab('show')
+})
+$('#toppings-tab-btn').on('click', function (e) {
+    e.preventDefault()
+    $('#myTab li:nth-child(3) a').tab('show')
+})
+$('#to-toppings').on('click', function (e) {
+    e.preventDefault()
+    $('#myTab li:nth-child(3) a').tab('show')
