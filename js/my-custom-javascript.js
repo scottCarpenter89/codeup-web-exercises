f9df43d8ce3dd463d3be93f019fe57fb4d709a26
Scott Carpenter <carpenter.scott22@gmail.com>
2022-02-03 13:47:50 -0600

    finished selectors exercises

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,13 +1,30 @@
 "use scrict";
 
+/* Assigning two ids for two different elements only displays the contents for the first and returns undefined for the
+second id assigned the id*/
 
+// var contents = $('#welcome').html();
+// alert(contents);
+//
+// var whoYou = $('#what-we-bout').html();
+// alert(whoYou);
+//
+// $(document).ready(function () {
+//     alert('The DOM has finished loading!');
+// })
 
-var contents = $('#welcome').html();
-alert(contents);
+// $('.codeup').css('border', '1px solid red');
 
-var whoYou = $('#what-we-bout').html();
-alert(whoYou);
+$('li').css('font-size', '20px');
 
-$(document).ready(function () {
-    alert('The DOM has finished loading!');
-})
+var welcome = $('#welcome').html();
+alert(welcome);
+
+var whoWeAre = $('#what-we-bout').html();
+alert(whoWeAre);
+
+var featuredNums = $('#elvish-bread').html();
+alert('Here is our featured item for tonight: ' + featuredNums);
+
+$('#welcome, #what-we-bout, .menu-item').css('background', 'yellow');
+
