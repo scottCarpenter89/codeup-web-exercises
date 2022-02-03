d24d1de16a49ef30fa8a08b754b813aba842d49f
Scott Carpenter <carpenter.scott22@gmail.com>
2022-02-03 11:05:13 -0600

    added two jQuery alerts

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,4 +1,13 @@
 "use scrict";
+
+
+
+var contents = $('#welcome').html();
+alert(contents);
+
+var whoYou = $('#what-we-bout').html();
+alert(whoYou);
+
 $(document).ready(function () {
     alert('The DOM has finished loading!');
 })
