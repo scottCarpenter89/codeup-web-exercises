adaff8915d5a60c6275a46260f2228827b51970a
Scott Carpenter <carpenter.scott22@gmail.com>
2022-02-04 08:45:05 -0600

    added hover color to li elements

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -39,3 +39,11 @@ $('p').dblclick(
     function () {
         $(this).css('font-size', '18px');
     });
+
+$('li').hover(
+    function () {
+        $(this).css('color', 'red');
+    },
+    function () {
+        $(this).css('color', 'black');
+    });
