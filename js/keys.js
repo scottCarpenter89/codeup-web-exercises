7d31868058c5c933ce536dc9835d42e20dde6fa8
Scott Carpenter <carpenter.scott22@gmail.com>
2022-02-10 16:26:04 -0600

    added tomball weather data

--- js/keys.js
+++ js/keys.js
@@ -1,3 +1,3 @@
 "use strict";
 const SCOTTIE_MAPBOX_API = "pk.eyJ1Ijoic2NvdHQtY2FycGVudGVyIiwiYSI6ImNremVtZjdyZzM5bGEycHIxazFzaWg4aWwifQ.npJuQ-xwxvzP8vERdVfJQA"
-const SCOTT_WEATHERMAP_API = "3607a0b56d6a03983aeeedba48678d5c"
+const SCOTT_WEATHERMAP_API = '3607a0b56d6a03983aeeedba48678d5c'
