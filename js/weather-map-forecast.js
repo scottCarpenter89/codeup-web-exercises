e0be5392020ab5bf60168787c14c62d7d7a6ce2a
Scott Carpenter <carpenter.scott22@gmail.com>
2022-02-11 17:12:29 -0600

    EOD save. Refactor & replan tomorrow

---
+++ js/weather-map-forecast.js
@@ -0,0 +1,12 @@
+"use strict";
+// $(document).on('ready', function () {
+//     const STARTING_COORDS = [-95.616203, 30.097191];
+//     const tomballLat = 30.097191
+//     const tomballLon = -95.616203
+//     const optimalZoom = 15;
+//     // let searchQuery = $('#city-search').
+//     $.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${tomballLat}&lon=${tomballLon}&appid=${SCOTT_WEATHERMAP_API}`, {}).done(function (data) {
+//         console.log(data);
+//     });
+
+// });
