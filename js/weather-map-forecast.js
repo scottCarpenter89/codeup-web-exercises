4cba6ed9241b42055b1d56f473799829baf82970
Scott Carpenter <carpenter.scott22@gmail.com>
2022-02-14 17:59:49 -0600

    attempted to refactor tab pane

--- js/weather-map-forecast.js
+++ js/weather-map-forecast.js
@@ -1,42 +1,55 @@
 "use strict";
-$('#day0 a').on('click', function (event) {
+$('#day0 a').on('load', function (event) {
     event.preventDefault()
-    $(this).tab('show')
+
     $('#day1 a').tab('hide')
     $('#day2 a').tab('hide')
     $('#day3 a').tab('hide')
     $('#day4 a').tab('hide')
+    $(this).tab('show')
+})
+$('#day0 a').on('click', function (event) {
+    event.preventDefault()
+
+    $('#day1 a').tab('hide')
+    $('#day2 a').tab('hide')
+    $('#day3 a').tab('hide')
+    $('#day4 a').tab('hide')
+    $(this).tab('show')
 });
 $('#day1 a').on('click', function (event) {
     event.preventDefault()
-    $(this).tab('show')
+
     $('#day0 a').tab('hide')
     $('#day2 a').tab('hide')
     $('#day3 a').tab('hide')
     $('#day4 a').tab('hide')
+    $(this).tab('show')
 });
 $('#day2 a').on('click', function (event) {
     event.preventDefault()
-    $(this).tab('show')
+
     $('#day0 a').tab('hide')
     $('#day1 a').tab('hide')
     $('#day3 a').tab('hide')
     $('#day4 a').tab('hide')
+    $(this).tab('show')
 });
 $('#day3 a').on('click', function (event) {
     event.preventDefault()
-    $(this).tab('show')
+
     $('#day0 a').tab('hide')
     $('#day1 a').tab('hide')
     $('#day2 a').tab('hide')
     $('#day4 a').tab('hide')
-
+    $(this).tab('show')
 });
 $('#day4 a').on('click', function (event) {
     event.preventDefault()
-    $(this).tab('show')
+
     $('#day0 a').tab('hide')
     $('#day1 a').tab('hide')
     $('#day2 a').tab('hide')
     $('#day3 a').tab('hide')
+    $(this).tab('show')
 })
